b30e865ecfbd273d4940245650caaa5e1f33c9f2
Alexander Koryagin <alexander.koryagin@tecswepoint.se>
2017-04-26 17:03:52 +0300

    Add app

--- site/simple_site/settings.py
+++ site/simple_site/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'simple_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
         'HOST': 'db',
